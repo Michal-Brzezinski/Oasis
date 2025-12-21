1de10584aaea7338c3b93adf21da13bce34f9dcd
Michal Brzezinski <michal.brzezinski.off@gmail.com>
2025-12-21 11:55:10 +0100

    better view login

---
+++ public/scripts/login.js
@@ -0,0 +1,9 @@
+document.addEventListener("DOMContentLoaded", () => {
+  const togglePassword = document.getElementById('togglePassword');
+  const passwordInput = document.getElementById('password');
+
+  togglePassword.addEventListener('click', () => {
+    const type = passwordInput.getAttribute('type') === 'password' ? 'text' : 'password';
+    passwordInput.setAttribute('type', type);
+  });
+});
